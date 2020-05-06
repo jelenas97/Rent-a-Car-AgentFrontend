24bfd68b2603e4d43ee3ce40bf8a6495db5acdb8
SaraFojkar <56953463+SaraFojkar@users.noreply.github.com>
2020-05-06 22:59:04 +0200

    updated RentRequests component

--- src/app/client-profile/requested-rents/requested-rents.component.ts
+++ src/app/client-profile/requested-rents/requested-rents.component.ts
@@ -1,9 +1,8 @@
+import { RentRequest } from './../../shared/model/rent-request';
+import { RentRequestService } from './../../service/rent-request.service';
 import { CancelRentDialogComponent } from './cancel-rent-dialog/cancel-rent-dialog.component';
-import { CommentCarDialogComponent } from '../history-rents/comment-car-dialog/comment-car-dialog.component';
-import { CarRatingDialogComponent } from '../history-rents/car-rating-dialog/car-rating-dialog.component';
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material';
-import {Advertisement} from '../../shared/model/advertisement';
 
 @Component({
   selector: 'app-requested-rents',
@@ -12,33 +11,33 @@ import {Advertisement} from '../../shared/model/advertisement';
 })
 export class RequestedRentsComponent implements OnInit {
 
-  _clientId: any =1;
-  advertisements: Advertisement[] = [
-    { id: 1, kilometresLimit: 200, discount: 10, cwd: true, image: "https://pbs.twimg.com/profile_images/588433651144196096/nCXD0GOf_400x400.jpg", mileage: 4000, kidSeats: 0, availableTracking: true, carClass: "SUV", carBrand: "BMW", price: 1, transmissionType: "automatic", fuelType: "petrol", rate: 1, name: "Car A", model: "ddw" },
-    { id: 4, kilometresLimit: 200, discount: 10, cwd: true, image: "https://pbs.twimg.com/profile_images/588433651144196096/nCXD0GOf_400x400.jpg", mileage: 20000, kidSeats: 0, availableTracking: true, carClass: "SUV", carBrand: "BMW", price: 4, transmissionType: "automatic", fuelType: "petrol", rate: 5, name: "Car A", model: "ddw" },
-    { id: 2, kilometresLimit: 200, discount: 10, cwd: true, image: "https://pbs.twimg.com/profile_images/739172838100328448/bm6PG0Bv.jpg", mileage: 3000, kidSeats: 0, availableTracking: true, carClass: "SUV", carBrand: "BMW", price: 3, transmissionType: "automatic", fuelType: "petrol", rate: 3, name: "Car B", model: "ddw" },
-    { id: 3, kilometresLimit: 200, discount: 10, cwd: true, image: "https://image.made-in-china.com/2f0j00zFJQUkHsbDpb/4-Wheel-Small-Electric-Car-Professional-Design-2-Doors.jpg", mileage: 50000, kidSeats: 0, availableTracking: true, carClass: "SUV", carBrand: "BMW", price: 2, transmissionType: "automatic", fuelType: "petrol", rate: 3, name: "Car A", model: "ddw" },
-    { id: 5, kilometresLimit: 200, discount: 10, cwd: true, image: "https://www.pvnautoparts.com/image/pvnautoparts/image/cache/data/Car%20Photos/cNBc5ZMb1568434447-256x256.jpg", mileage: 1000, kidSeats: 0, availableTracking: true, carClass: "SUV", carBrand: "BMW", price: 5, transmissionType: "automatic", fuelType: "petrol", rate: 3, name: "Car A", model: "ddw" },
-  ];
+  _clientId: any =8;
+  _rentRequests: RentRequest[];
 
-
-  constructor(private _dialog: MatDialog) { }
+  constructor(private _dialog: MatDialog, private _rentRequestService : RentRequestService) { }
 
   ngOnInit(): void {
-
-   /* this._route.paramMap.subscribe(params => { this._customerId = params.get('id'); });*/
+    this.getRentRequests(this._clientId);
+  
   }
 
-  cancelRent(advertisement){
+  cancelRent(rentRequest){
     let dialog = this._dialog.open(CancelRentDialogComponent, {
       width: '30%',
-      data: {_advertisement : advertisement, _advertisements : this.advertisements, _clientId : this._clientId},
+      data: {_rentRequest : rentRequest, _rentRequests : this._rentRequests, _clientId : this._clientId},
 
     });
     dialog.afterClosed().subscribe(data => {
-      this.advertisements=data;
+      this._rentRequests=data;
     });
 
+  }
+
+  getRentRequests(id){
+    this._rentRequestService.getRentRequests(id).subscribe(rentRequests=>{
+      console.log(rentRequests);
+      this._rentRequests=rentRequests;
+    })
   }
 
 }
