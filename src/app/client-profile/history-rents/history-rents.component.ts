b3da586ce656ef52fe8a001990ad59693a27e65d
SaraFojkar <56953463+SaraFojkar@users.noreply.github.com>
2020-06-01 01:57:05 +0200

    updated history rents (real client))

--- src/app/client-profile/history-rents/history-rents.component.ts
+++ src/app/client-profile/history-rents/history-rents.component.ts
@@ -1,3 +1,5 @@
+import { AuthService } from './../../service/auth.service';
+import { User } from './../../shared/model/user';
 import { RentRequestService } from './../../service/rent-request.service';
 import { RentRequest } from './../../shared/model/rent-request';
 import { CarRatingDialogComponent } from './car-rating-dialog/car-rating-dialog.component';
@@ -14,20 +16,22 @@ import { MatDialog } from '@angular/material';
 })
 export class HistoryRentsComponent implements OnInit {
 
-  _clientId: any =8;
+  currUser : User;
   _historyRentRequests : RentRequest[];
 
 
-  constructor(private _dialog: MatDialog,  private _rentRequestService : RentRequestService) { }
+  constructor(private _dialog: MatDialog,  private _rentRequestService : RentRequestService,
+      private authService: AuthService) { }
 
   ngOnInit(): void {
-    this.getHistoryRentRequests(this._clientId);
+    this.currUser=this.authService.getCurrUser();
+    this.getHistoryRentRequests(this.authService.getCurrUser().id);
   }
 
   rateCar(historyRentRequest) {
     let dialog = this._dialog.open(CarRatingDialogComponent, {
       width: '30%',
-      data: { _historyRentRequest : historyRentRequest,  _clientId : this._clientId}
+      data: { _historyRentRequest : historyRentRequest,  _clientId : this.currUser.id}
     });
     dialog.afterClosed().subscribe(data => {
 
@@ -37,7 +41,7 @@ export class HistoryRentsComponent implements OnInit {
   commentCar(historyRentRequest) {
     let dialog = this._dialog.open(CommentCarDialogComponent, {
       width: '30%',
-      data:  { _historyRentRequest : historyRentRequest,  _clientId : this._clientId}
+      data:  { _historyRentRequest : historyRentRequest,  _clientId : this.currUser.id}
 
     });
     dialog.afterClosed().subscribe(data => {
