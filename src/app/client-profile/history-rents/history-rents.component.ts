eda2f9d5181074e45111bed2ba5ec2ccfcc7e791
SaraFojkar <56953463+SaraFojkar@users.noreply.github.com>
2020-06-03 00:53:36 +0200

    updated history rents component

--- src/app/client-profile/history-rents/history-rents.component.ts
+++ src/app/client-profile/history-rents/history-rents.component.ts
@@ -1,3 +1,4 @@
+import { NotifierService } from 'angular-notifier';
 import { AuthService } from './../../service/auth.service';
 import { User } from './../../shared/model/user';
 import { RentRequestService } from './../../service/rent-request.service';
@@ -5,8 +6,9 @@ import { RentRequest } from './../../shared/model/rent-request';
 import { CarRatingDialogComponent } from './car-rating-dialog/car-rating-dialog.component';
 import { CommentCarDialogComponent } from './comment-car-dialog/comment-car-dialog.component';
 import { Advertisement } from '../../shared/model/advertisement';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialog } from '@angular/material';
+import { DOCUMENT } from '@angular/common'; 
 
 
 @Component({
@@ -18,10 +20,10 @@ export class HistoryRentsComponent implements OnInit {
 
   currUser : User;
   _historyRentRequests : RentRequest[];
-
+  remove: boolean;
 
   constructor(private _dialog: MatDialog,  private _rentRequestService : RentRequestService,
-      private authService: AuthService) { }
+      private authService: AuthService, private notifier : NotifierService, @Inject(DOCUMENT) document) { }
 
   ngOnInit(): void {
     this.currUser=this.authService.getCurrUser();
@@ -33,9 +35,7 @@ export class HistoryRentsComponent implements OnInit {
       width: '30%',
       data: { _historyRentRequest : historyRentRequest,  _clientId : this.currUser.id}
     });
-    dialog.afterClosed().subscribe(data => {
-
-    });
+    dialog.afterClosed().subscribe();
   }
 
   commentCar(historyRentRequest) {
@@ -45,13 +45,13 @@ export class HistoryRentsComponent implements OnInit {
 
     });
     dialog.afterClosed().subscribe(data => {
-
+      console.log(data);
     });
   }
 
   getHistoryRentRequests(id:string){
     this._rentRequestService.getHistoryRentRequests(id).subscribe(historyRentRequests=> {
-      console.log(historyRentRequests);
+      console.log(historyRentRequests.length);
       this._historyRentRequests=historyRentRequests;
     })
   }
