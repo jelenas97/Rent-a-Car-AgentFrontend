08c739b018bc2dff544a76a698bf125af8557146
SaraFojkar <56953463+SaraFojkar@users.noreply.github.com>
2020-06-14 18:11:32 +0200

    updated rate and comment model

--- src/app/shared/model/comment.ts
+++ src/app/shared/model/comment.ts
@@ -1,8 +1,10 @@
 export class Comment {
     id: any;
     client_id: any;
+    client: any;
     content: String;
     date: any;
+    dateString:any;
     advertisement_id: any;
     status: any;
     rent_request_id:any;
