c7278bbf76dbac9a526e9b575dc87fc430ae305e
SaraFojkar <56953463+SaraFojkar@users.noreply.github.com>
2020-06-17 11:56:31 +0200

    Create commentOwner.ts

---
+++ src/app/shared/model/commentOwner.ts
@@ -0,0 +1,12 @@
+export class CommentOwner {
+    id: any;
+    commenter_id: any;
+    commenter: any;
+    content: String;
+    date: any;
+    dateString:any;
+    advertisement_id: any;
+    status: any;
+
+    constructor(){}
+}
